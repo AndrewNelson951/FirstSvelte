808748eafd207dc0a7dcdafbfcb4eae63dbdf248
Andrew Nelson <AndrewNelson951@gmail.com>
2024-10-02 00:22:54 -0400

    Templating with if and each loops

--- App.svelte
+++ App.svelte
@@ -1,20 +1,23 @@
 <script>
     import Face from './Face.svelte';
     import Container from './Container.svelte';
-    let say = 'hi';
+    let say = false;
     setTimeout(() => {
-        say = 'bye';
+        say = true;
     }, 1000)
 </script>
 
 <Container>
-    <div>
-        Say: {say}
-    </div>
-
-    <Face index={0} />
-    <Face />
-    <Face index={2}/>
+    {#if say}
+        <div>
+            Hi!
+        </div>
+    {:else if !say}
+        not saying anything..
+    {/if}
+    {#each [2,1,0] as index}
+        <Face {index} />
+    {/each}
 </Container>
 
 
