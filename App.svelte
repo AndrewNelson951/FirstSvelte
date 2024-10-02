7fe8aae2e9a4b2d5f5734bb1e2b0b0d4f1c7206a
Andrew Nelson <AndrewNelson951@gmail.com>
2024-10-02 00:36:38 -0400

    Buttons updating a score

--- App.svelte
+++ App.svelte
@@ -4,6 +4,13 @@
     import Header from './Header.svelte';
     import Buttons from './Buttons.svelte';
     let showHeader = false;
+    
+    const buttons = [
+        {value: 0,  text: 'ummmmmm......'},
+        {value: 1,  text: 'I sure do!'},
+        {value: -2, text: 'gross!'}
+    ]
+    let score = 0;
 </script>
 
 
@@ -11,8 +18,18 @@
     <Header />
 {/if}
 <Container>
-    <Buttons on:click={(e) => {showHeader = e.detail}} />
+    Do you like pizza? Score: {score}
+    <Buttons {buttons} on:click={(e) => {score += e.detail.value}} />
 </Container>
+
+<!-- Challenge 3 - 
+1. add a prop in Buttons.svelte called buttons which is a list of objects like:
+[{value: '', text: ''}, ...etc]
+2. use #each to turn all the objects into buttons that
+    a. have innerHTML equal to the .text of the object
+    b. dispatch a click event that passes the .value of the object 
+3. Handle the event in App.svelte to update the score
+-->
 
 <style>
     div {
